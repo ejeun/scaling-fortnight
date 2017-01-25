ea2b6ad5aed31c35759721830408bd498f37130e
jeunlee <junniesu@gmail.com>
2017-01-25 14:54:58 -0500

    cats in seed file, need to write tests

--- db/seed.js
+++ db/seed.js
@@ -1,13 +1,21 @@
 const db = require('APP/db')
 
-const seedUsers = () => db.Promise.map([
-  {name: 'so many', email: 'god@example.com', password: '1234'},
-  {name: 'Barack Obama', email: 'barack@example.gov', password: '1234'},
-], user => db.model('users').create(user))
+const seedCats = () => db.Promise.map([
+  {
+    name: 'Meow',
+    likes: ['bananas', 'hotdogs', 'ovals'],
+    dislikes: ['boxes', 'dice', 'cubes'],
+  },
+  {
+    name: 'Miao',
+    likes: ['people', 'fashion', 'woman'],
+    dislikes: ['red', 'building', 'outdoor'],
+  }
+  ], cat => db.model('cats').create(cat))
 
 db.didSync
   .then(() => db.sync({force: true}))
-  .then(seedUsers)
-  .then(users => console.log(`Seeded ${users.length} users OK`))
-  .catch(error => console.error(error))    
+  .then(seedCats)
+  .then(cats => console.log(`Seeded ${cats.length} cats OK`))
+  .catch(error => console.error(error))
   .finally(() => db.close())
