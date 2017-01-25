ed617c609939b7e4ace78eb70aa413fa79e9b614
jeunlee <junniesu@gmail.com>
2017-01-25 14:36:54 -0500

    made cat model and association between user

--- db/models/index.js
+++ db/models/index.js
@@ -4,6 +4,9 @@
 // so any other part of the application could call sequelize.model('User')
 // to get access to the User model.
 
-const User = require('./user')
+const User = require('./user');
+const Cat = require('./cat');
 
-module.exports = {User}
+Cat.belongsTo(User, {as: 'pet'});
+
+module.exports = {User, Cat};
