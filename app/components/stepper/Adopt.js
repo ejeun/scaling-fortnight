19c6731d90be620b03ef4928d3615af73d916706
jeunlee <junniesu@gmail.com>
2017-01-26 18:28:02 -0500

    create account and paperwork forms being created

--- app/components/stepper/Adopt.js
+++ app/components/stepper/Adopt.js
@@ -21,7 +21,43 @@ class Adopt extends React.Component {
   render() {
     return(
       <div className="">
-
+        <h2>A few important questions...</h2>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <TextField
+                name="color"
+                hintText="Color"
+                floatingLabelText="Provide your favorite color"
+                onChange={this.handleChangeName}
+              /><br />
+              <br />
+              <TextField
+                name="food"
+                hintText="Food"
+                floatingLabelText="Share the name of the best dish you can cook"
+                onChange={this.handleChangeEmail}
+              /><br />
+              <br />
+              <TextField
+                name="cat"
+                hintText="Cat"
+                floatingLabelText="Describe your favorite quality of cats in one word"
+                type="password"
+                errorText={this.errorText}
+                onChange={this.handleChangeConfirm}
+              /><br />
+              <br />
+              <TextField
+                name="experience"
+                hintText="Experience"
+                floatingLabelText="How many years experience do you have talking to cats?"
+                type="password"
+                onChange={this.handleChangePassword}
+              /><br />
+              <br />
+{/*                  <RaisedButton type="submit" value="signUp" label="Sign Up" disabled={this.state.disabled} labelStyle={{color: 'white'}}/>*/}
+            </div>
+          </form>
       </div>)
    }
 }
--- app/components/stepper/CreateUser.js
+++ app/components/stepper/CreateUser.js
@@ -7,6 +7,8 @@ import ChevronLeft from 'material-ui/svg-icons/navigation/chevron-left';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import FontIcon from 'material-ui/FontIcon';
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 
 /* -----------------    COMPONENT     ------------------ */
@@ -17,13 +19,50 @@ class CreateUser extends React.Component {
     super(props)
   }
 
-
   render() {
     return(
       <div className="">
 
-      </div>)
-   }
+              <h2>Sign Up</h2>
+              <form onSubmit={this.handleSubmit}>
+                <div>
+                  <TextField
+                    name="name"
+                    hintText="Name"
+                    floatingLabelText="Name"
+                    onChange={this.handleChangeName}
+                  /><br />
+                  <br />
+                  <TextField
+                    name="email"
+                    hintText="Email"
+                    floatingLabelText="Email"
+                    onChange={this.handleChangeEmail}
+                  /><br />
+                  <br />
+                  <TextField
+                    name="password"
+                    hintText="Password"
+                    floatingLabelText="Password"
+                    type="password"
+                    onChange={this.handleChangePassword}
+                  /><br />
+                  <br />
+                  <TextField
+                    name="passwordConfirm"
+                    hintText="Confirm Password"
+                    floatingLabelText="Confirm Password"
+                    type="password"
+                    errorText={this.errorText}
+                    onChange={this.handleChangeConfirm}
+                  /><br />
+                  <br />
+{/*                  <RaisedButton type="submit" value="signUp" label="Sign Up" disabled={this.state.disabled} labelStyle={{color: 'white'}}/>*/}
+                </div>
+              </form>
+      </div>
+    )
+  }
 }
 
 
